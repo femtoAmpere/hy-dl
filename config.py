59f53255b72d681b8fc7890cc8f3b7e9388957ef
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2026-03-29 19:16:30 +0200

    works on my machine

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 
+import os
 import subprocess
 
-version = subprocess.check_output(['git', 'describe', '--tags', '--dirty', '--long']).strip().decode('utf-8')
+telegram_token = 'your_botfather_token_here'
+telegram_users = [0, 1, 2]
 
-with open('.telegram_bot_token', 'r') as f:
-    telegram_token = f.read().strip()
+cifs_user = 'smb_user'
+cifs_pass = 'smb_pass'
+cifs_endpoint = '//some-server.domain/share/downloads'
+
+downloads = os.path.join('.', 'downloads')
+
+version = subprocess.check_output(['git', 'describe', '--tags', '--dirty', '--long']).strip().decode('utf-8')
+last_updated_hour = -1
--- main.py
+++ main.py
@@ -1,13 +1,74 @@
 
+import cmd
+from turtle import update
+
 import config
+import os
 
-import gallery_dl
+from datetime import datetime
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
+import subprocess
+
+def download_gallery_dl(url: str, updateDownloaders=False) -> str:
+    try:
+        cmd = ''
+        if updateDownloaders:
+            cmd += subprocess.check_output([r'.venv\Scripts\python' if os.name == 'nt' else '.venv/bin/python', '-m', 'pip', 'install', '--upgrade', 'pip', 'gallery-dl'], shell=False, text=True, cwd=config.downloads)
+        cmd += subprocess.check_output([r'.venv\Scripts\gallery-dl' if os.name == 'nt' else '.venv/bin/gallery-dl', url], shell=False, text=True, cwd=config.downloads)
+        return f'**gallery-dl**\n```\n{cmd}\n```'
+    except subprocess.CalledProcessError as e:
+        return f'**gallery-dl** error {e.returncode}:\n```\n{e.output}\n```'
+    except Exception as e:
+        return f'**gallery-dl** error: {e}'
+    
+    return cmd
+
+def download_yt_dlp(url: str, updateDownloaders=False) -> str:
+    try:
+        cmd = ''
+        if updateDownloaders:
+            cmd += subprocess.check_output([os.path.join('..', 'yt-dlp'), '--update-to', 'nightly'], shell=True, text=True, cwd=os.path.join(config.downloads, 'yt-dlp'))
+        cmd += subprocess.check_output([os.path.join('..', 'yt-dlp'), url], shell=True, text=True, cwd=os.path.join(config.downloads, 'yt-dlp'))
+        return f'**yt-dlp**\n```\n{cmd}\n```'
+    except subprocess.CalledProcessError as e:
+        return f'**yt-dlp** error {e.returncode}:\n```\n{e.output}\n```'
+    except Exception as e:
+        return f'**yt-dlp** error: {e}'
+    
+    return cmd
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'You said: {update.message.text}')
+    if update.message.from_user.id not in config.telegram_users:
+        await update.message.reply_text('Unauthorized user.')
+        return
+    
+    if not update.message.text.lower().startswith(('http://', 'https://')):
+        await update.message.reply_text('Please send a valid URL starting with http:// or https://')
+        return
+    
+    url = update.message.text
+    if 'furaffinity.net' in url.lower():
+        with open(os.path.join(config.downloads, 'FurAffinity.txt'), 'a+') as f:
+            f.write(url + '\n')
+        await update.message.reply_text(f'+FurAffinity.txt: `{url}`', parse_mode='Markdown')
+        return
+    
+    with open(os.path.join(config.downloads, 'hydrus-import.txt'), 'a+') as f:
+        f.write(url + '\n')
+    await update.message.reply_text(f'+hydrus-import.txt: `{url}`', parse_mode='Markdown')
+    
+    hour, do_update = datetime.now().hour, False
+    if (hour - config.last_updated_hour) > 1: 
+        do_update = True
+        config.last_updated_hour = hour
+    
+    gdl = download_gallery_dl(url, updateDownloaders=do_update)
+    await update.message.reply_text(gdl, parse_mode='Markdown')
+
+    ytdlp = download_yt_dlp(url, updateDownloaders=do_update)
+    await update.message.reply_text(ytdlp, parse_mode='Markdown')
 
 
 app = ApplicationBuilder().token(config.telegram_token).build()
