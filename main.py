770514fd3641b5148ced95006729eb550db71f75
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2026-04-01 00:28:28 +0200

    downloading list of urls remount download dir

--- main.py
+++ main.py
@@ -12,78 +12,112 @@ import subprocess
 last_downloaders_update = datetime.datetime.min
 
 
-def download_gallery_dl(url: str, update_downloader=False) -> str:
+def sh_mount(fspath=config.downloads):
+    cmd = ''
+
+    try:
+        cmd += subprocess.check_output(['umount', fspath], shell=False, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(f'*WARNING* Failed to unmount target with {e.returncode}. Output: {e.output}')
+    except Exception as e:
+        return e.returncode, f'*ERROR* Failed to unmount target with {e.returncode}. Output: {e.output}'
+    
+    try:
+        cmd += subprocess.check_output(['mount', fspath], shell=False, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        return e.returncode, f'*WARNING* Failed to mount target with {e.returncode}. Output: {e.output}'
+    except Exception as e:
+        return e.returncode, f'*ERROR* Failed to mount target with {e.returncode}. Output: {e.output}'
+
+    return 0, cmd
+
+def sh_download_gallery_dl(url: str, update_downloader=False) -> str:
     try:
         cmd = ''
         if update_downloader:
             cmd += subprocess.check_output(['downloaders/gallery-dl/bin/python', '-m', 'pip', 'install', '--upgrade', 'pip', 'gallery-dl'], shell=False, text=True, stderr=subprocess.STDOUT)
         cmd += subprocess.check_output(['downloaders/gallery-dl/bin/gallery-dl', '--config', '.gallery-dl.conf', '--dest', f'{config.downloads}/gallery-dl', url], shell=False, text=True, stderr=subprocess.STDOUT)
-        return f'+**gallery-dl**\n```\n{cmd}\n```\n'
+        return 0, f'+**gallery-dl**\n```\n{cmd}\n```\n'
     except subprocess.CalledProcessError as e:
-        return f'-**gallery-dl** error {e.returncode}:\n```\n{e.output}\n```\n'
+        return e.returncode, f'-**gallery-dl** error {e.returncode}:\n```\n{e.output}\n```\n'
     except Exception as e:
-        return f'-**gallery-dl** error {e}\n'
+        return e.returncode, f'-**gallery-dl** error {e}\n'
     
-    return cmd
+    return 0, cmd
 
-def download_yt_dlp(url: str, update_downloader=False) -> str:
+def sh_download_yt_dlp(url: str, update_downloader=False) -> str:
     try:
         cmd = ''
         if update_downloader:
             cmd += subprocess.check_output(['downloaders/yt-dlp', '--update-to', 'nightly'], shell=False, text=True, stderr=subprocess.STDOUT)
         cmd += subprocess.check_output(['../../downloaders/yt-dlp', url], shell=False, text=True, cwd=os.path.join(config.downloads, 'yt-dlp'), stderr=subprocess.STDOUT)
-        return f'+**yt-dlp**\n```\n{cmd}\n```\n'
+        return 0, f'+**yt-dlp**\n```\n{cmd}\n```\n'
     except subprocess.CalledProcessError as e:
-        return f'-**yt-dlp** error {e.returncode}:\n```\n{e.output}\n```\n'
+        return e.returncode, f'-**yt-dlp** error {e.returncode}:\n```\n{e.output}\n```\n'
     except Exception as e:
-        return f'-**yt-dlp** error {e}\n'
+        return e.returncode, f'-**yt-dlp** error {e}\n'
     
-    return cmd
+    return 0, cmd
+
+async def download(urls: str, update_downloader=False) -> str:
+
+    ret, mounted = sh_mount()
+    if ret != 0:
+        return mounted
+
+    msg = ''
+
+    for url in urls:
+        url = url.strip()
+
+        msg += f'--------\nDownload Receipt for `{url}`\n\n'
+
+        if not url.lower().startswith(('http://', 'https://')):
+            msg += 'Please send a valid URL starting with http:// or https://\n'
+            continue
+        
+        if 'furaffinity.net' in url.lower():
+            with open(os.path.join(config.downloads, 'FurAffinity.txt'), 'a+') as f:
+                f.write(url + '\n')
+            msg += f'+FurAffinity.txt: `{url}`'
+            continue
+
+        with open(os.path.join(config.downloads, 'hydrus-import.txt'), 'a+') as f:
+            f.write(url + '\n')
+        msg += f'+hydrus-import.txt: `{url}`\n\n'
+
+        any_downloader_success = False
+
+        ret, gdl = sh_download_gallery_dl(url, update_downloader=update_downloader)
+        if ret == 0: any_downloader_success = True
+        msg += gdl
+
+        ret, ytdlp = sh_download_yt_dlp(url, update_downloader=update_downloader)
+        if ret == 0: any_downloader_success = True
+        msg += ytdlp
+
+        if not any_downloader_success:
+            with open(os.path.join(config.downloads, 'failed.txt'), 'a+') as f:
+                f.write(url + '\n')
+            msg += f'+failed.txt: `{url}`\n\n'
+        
+    return msg
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message.from_user.id not in config.telegram_users:
         await update.message.reply_text('Unauthorized user.')
         return
     
-    if not update.message.text.lower().startswith(('http://', 'https://')):
-        await update.message.reply_text('Please send a valid URL starting with http:// or https://')
-        return
-    
-    url = update.message.text.strip()
-    msg = f'Download Report for `{url}`\n\n'
-    
-    if 'furaffinity.net' in url.lower():
-        with open(os.path.join(config.downloads, 'FurAffinity.txt'), 'a+') as f:
-            f.write(url + '\n')
-        msg += f'+FurAffinity.txt: `{url}`'
-        await update.message.reply_text(msg, parse_mode='Markdown')
-        return
-
-    with open(os.path.join(config.downloads, 'hydrus-import.txt'), 'a+') as f:
-        f.write(url + '\n')
-    msg += f'+hydrus-import.txt: `{url}`\n\n'
-    
     now, update_pending = datetime.datetime.now(), False
     global last_downloaders_update
     if (now - last_downloaders_update).total_seconds() > 60*60*2:
         update_pending = True
         last_downloaders_update = now
-    
-    any_downloader_success = False
 
-    gdl = download_gallery_dl(url, update_downloader=update_pending)
-    if gdl.startswith('+'): any_downloader_success = True
-    msg += gdl
+    urls = update.message.text.strip().split('\n')
+    await update.message.reply_text(f'Downloading {len(urls)} URLs...', parse_mode='Markdown')
 
-    ytdlp = download_yt_dlp(url, update_downloader=update_pending)
-    if ytdlp.startswith('+'): any_downloader_success = True
-    msg += ytdlp
-
-    if not any_downloader_success:
-        with open(os.path.join(config.downloads, 'failed.txt'), 'a+') as f:
-            f.write(url + '\n')
-        msg += f'+failed.txt: `{url}`\n\n'
-
+    msg = await download(update.message.text, update_downloader=update_pending)
     await update.message.reply_text(msg, parse_mode='Markdown')
 
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
