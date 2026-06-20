fc89c40616978a3fd14fed284c3a038b158fb1aa
femtoAmpere <3886111+femtoAmpere@users.noreply.github.com>
2026-06-20 21:12:07 +0200

    async refactor

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import io
 import datetime
 import asyncio
+import subprocess
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
@@ -11,66 +12,65 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Con
 
 last_downloaders_update = datetime.datetime.min
 
-async def sh_mount(fspath=config.downloads):
+def sh_mount(fspath=config.downloads):
     cmd = ''
 
-    proc = await asyncio.create_subprocess_shell(f'umount {fspath}', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate()
-    cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
-    if proc.returncode != 0:
-        print(f'*WARNING* Failed to unmount target with {proc.returncode}. Output: {stderr}')
-        #return proc.returncode, stderr
+    try:
+        cmd += subprocess.check_output(['umount', fspath], shell=False, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        print(f'*WARNING* Failed to unmount target with {e.returncode}. Output: {e.output}')
+    except Exception as e:
+        return e.returncode, f'*ERROR* Failed to unmount target with {e.returncode}', e.output
     
-    proc = await asyncio.create_subprocess_shell(f'mount {fspath}', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate()
-    cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
-    if proc.returncode != 0:
-        print(f'*ERROR* Failed to mount target with {proc.returncode}. Output: {stderr}')
-        return proc.returncode, stderr, cmd
-        
+    try:
+        cmd += subprocess.check_output(['mount', fspath], shell=False, text=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        return e.returncode, f'*WARNING* Failed to mount target with {e.returncode}', e.output
+    except Exception as e:
+        return e.returncode, f'*ERROR* Failed to mount target with {e.returncode}', e.output
+
     return 0, cmd, ''
 
-async def sh_download_gallery_dl(url: str, update_downloader=False) -> str:
-    cmd = ''
-    if update_downloader:
-        proc = await asyncio.create_subprocess_shell(f'downloaders/gallery-dl/bin/python -m pip install --upgrade pip gallery-dl', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-        stdout, stderr = await proc.communicate()
-        cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
-    
-    proc = await asyncio.create_subprocess_shell(f'downloaders/gallery-dl/bin/gallery-dl --config config/.gallery-dl.conf --dest {config.downloads}/gallery-dl {url}', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate()
-    cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
+def sh_download_gallery_dl(url: str, update_downloader=False) -> str:
+    try:
+        cmd = ''
+        if update_downloader:
+            cmd += subprocess.check_output(['downloaders/gallery-dl/bin/python', '-m', 'pip', 'install', '--upgrade', 'pip', 'gallery-dl'], shell=False, text=True, stderr=subprocess.STDOUT)
+        cmd += subprocess.check_output(['downloaders/gallery-dl/bin/gallery-dl', '--config', 'conf/.gallery-dl.conf', '--dest', f'{config.downloads}/gallery-dl', url], shell=False, text=True, stderr=subprocess.STDOUT)
+        return 0, f'+**gallery-dl**\n', cmd
+    except subprocess.CalledProcessError as e:
+        return e.returncode, f'-**gallery-dl** error {e.returncode}\n', e.output
+    except Exception as e:
+        return e.returncode, f'-**gallery-dl** error {e}\n', e.output
 
-    return proc.returncode, f'+**gallery-dl**\n', cmd
-
-async def sh_download_yt_dlp(url: str, update_downloader=False) -> str:
+def sh_download_yt_dlp(url: str, update_downloader=False) -> str:
     os.makedirs(os.path.join(config.downloads, 'yt-dlp'), exist_ok=True)
+    try:
+        cmd = ''
+        if update_downloader:
+            cmd += subprocess.check_output(['downloaders/yt-dlp', '--update-to', 'nightly'], shell=False, text=True, stderr=subprocess.STDOUT)
+        cmd += subprocess.check_output(['../../downloaders/yt-dlp', url], shell=False, text=True, cwd=os.path.join(config.downloads, 'yt-dlp'), stderr=subprocess.STDOUT)
+        return 0, f'+**yt-dlp**\n', cmd
+    except subprocess.CalledProcessError as e:
+        return e.returncode, f'-**yt-dlp** error {e.returncode}\n', e.output
+    except Exception as e:
+        return e.returncode, f'-**yt-dlp** error {e}\n', e.output
 
-    cmd = ''
-    if update_downloader:
-        proc = await asyncio.create_subprocess_shell(f'downloaders/yt-dlp --update-to nightly', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-        stdout, stderr = await proc.communicate()
-        cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
-    
-    proc = await asyncio.create_subprocess_shell(f'../../downloaders/yt-dlp {url}', cwd=os.path.join(config.downloads, 'yt-dlp'))
-    stdout, stderr = await proc.communicate()
-    cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
-
-    return proc.returncode, f'+**yt-dlp**\n', cmd
-
-async def sh_download_megadl(url: str, update_downloader=False) -> str:
+def sh_download_megadl(url: str, update_downloader=False) -> str:
     os.makedirs(os.path.join(config.downloads, 'megadl'), exist_ok=True)
-    cmd = ''
-    if update_downloader:
-    #     cmd += await a_check_output(['downloaders/megadl', '--update-to', 'nightly'], shell=False, text=True, stderr=subprocess.STDOUT)
-        cmd += "No update possible for megadl.\n"
-    proc = await asyncio.create_subprocess_shell(f'megadl {url}', cwd=os.path.join(config.downloads, 'megadl'), stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await proc.communicate()
-    cmd += (stdout.decode() + '\n' if stdout else '') + (stderr.decode() + ' \n' if stderr else '')
-    
-    return proc.returncode, f'+**megadl**\n', cmd
+    try:
+        cmd = ''
+        if update_downloader:
+        #     cmd += subprocess.check_output(['downloaders/megadl', '--update-to', 'nightly'], shell=False, text=True, stderr=subprocess.STDOUT)
+            cmd += "No update possible for megadl.\n"
+        cmd += subprocess.check_output(f'megadl {url}', shell=True, text=True, cwd=os.path.join(config.downloads, 'megadl'), stderr=subprocess.STDOUT)
+        return 0, f'+**megadl**\n', cmd
+    except subprocess.CalledProcessError as e:
+        return e.returncode, f'-**megadl** error {e.returncode}\n', e.output
+    except Exception as e:
+        return e.returncode, f'-**megadl** error {e}\n', e.output
 
-async def e6w_bot_url_extract(update: Update, fallback: str = "") -> str:
+def e6w_bot_url_extract(update: Update, fallback: str = "") -> str:
     msg = update.message
     entities = getattr(msg, "caption_entities") or getattr(msg, "entities") or []
     for entity in entities:
@@ -79,11 +79,11 @@ async def e6w_bot_url_extract(update: Update, fallback: str = "") -> str:
     
     return fallback
 
-async def download(urls: str, update, update_downloader=False) -> str:
+def download(urls: list[str], update_downloader=False) -> list[str]:
 
-    ret, mounted, _ = await sh_mount()
+    ret, mounted, _ = sh_mount()
     if ret != 0:
-        return mounted
+        return [mounted]
 
     msgs = []
 
@@ -91,6 +91,7 @@ async def download(urls: str, update, update_downloader=False) -> str:
         url = url.strip()
 
         receipt = ''
+        docs = []
 
         receipt += f'Download Receipt for `{url}`\n\n'
 
@@ -111,43 +112,49 @@ async def download(urls: str, update, update_downloader=False) -> str:
         receipt += f'+hydrus-import.txt\n\n'
 
         if url.lower().startswith(('https://mega.nz/', 'mega.nz', 'www.mega.nz', 'https://www.mega.nz/')):
-            ret, msg, rio = await sh_download_megadl(url, update_downloader=update_downloader)
+            ret, msg, rio = sh_download_megadl(url, update_downloader=update_downloader)
             receipt += msg
             if rio and len(rio) > 0:
-                with io.StringIO(rio) as document:
-                    document.name = 'megadl.txt'
-                    await update.message.reply_document(document=document, caption='megadl receipt')
+                doc = io.StringIO(rio)
+                doc.name = 'megadl.txt'
+                docs.append(doc)
             if ret == 0:
                 msgs.append(receipt)
                 continue
 
         any_downloader_success = False
 
-        ret, msg, rio = await sh_download_gallery_dl(url, update_downloader=update_downloader)
+        ret, msg, rio = sh_download_gallery_dl(url, update_downloader=update_downloader)
         if ret == 0: any_downloader_success = True
         receipt += msg
         if rio and len(rio) > 0:
-            with io.StringIO(rio) as document:
-                document.name = 'gallery-dl.txt'
-                await update.message.reply_document(document=document, caption='gallery-dl receipt')
+            doc = io.StringIO(rio)
+            doc.name = 'gallery-dl.txt'
+            docs.append(doc)
 
-        ret, msg, rio = await sh_download_yt_dlp(url, update_downloader=update_downloader)
+        ret, msg, rio = sh_download_yt_dlp(url, update_downloader=update_downloader)
         if ret == 0: any_downloader_success = True
         receipt += msg
         if rio and len(rio) > 0:
-            with io.StringIO(rio) as document:
-                document.name = 'yt-dlp.txt'
-                await update.message.reply_document(document=document, caption='yt-dlp receipt')
+            doc = io.StringIO(rio)
+            doc.name = 'yt-dlp.txt'
+            docs.append(doc)
 
         if not any_downloader_success:
             with open(os.path.join(config.downloads, 'failed.txt'), 'a+') as f:
                 f.write(url + '\n')
             receipt += f'+failed.txt\n\n'
         
-        msgs.append(receipt)
+        msgs.append((receipt, docs))
 
     return msgs
 
+async def download_worker(urls: list[str], bot, chat_id: int, update_downloader=False):
+    messages = await asyncio.to_thread(download, urls, update_downloader)
+    for msg in messages:
+        await bot.send_message(chat_id=chat_id, text=msg[0], parse_mode='Markdown')
+        for doc in msg[1]:
+            await bot.send_document(chat_id=chat_id, document=doc, caption=f'{doc.name} receipt')
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message.from_user.id not in config.telegram_users:
@@ -171,9 +178,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
 
     await update.message.reply_text(acknowledge, parse_mode='Markdown')
 
-    messages = await download(urls, update, update_downloader=update_pending)
-    for msg in messages:
-        await update.message.reply_text(msg, parse_mode='Markdown')
+    context.application.create_task(download_worker(urls, context.bot, update.effective_chat.id, update_downloader=update_pending))
 
 
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
